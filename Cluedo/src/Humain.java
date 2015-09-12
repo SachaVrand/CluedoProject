b8b36db741c6cf22917bb305102dfb81df8a8a98
IceXs <utilisateur@192.168.1.31>
2015-09-12 17:11:00 +0200

    add to index humain ordi

---
+++ Cluedo/src/Humain.java
@@ -0,0 +1,9 @@
+
+public class Humain extends Joueur
+{
+	public Humain(String nom, String adresse)
+	{
+		super(nom, adresse);
+	}
+
+}
---
+++ Cluedo/src/Ordi.java
@@ -0,0 +1,8 @@
+
+public class Ordi extends Joueur
+{
+	public Ordi(String nom, String adresse)
+	{
+		super(nom, adresse);
+	}
+}
