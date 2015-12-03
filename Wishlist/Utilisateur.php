c12d9390d7c23e44588b351d8761abdbc0d958f5
Sacha <vrand.sacha@gmail.com>
2015-12-03 20:23:23 +0100

    modif

--- Wishlist/Utilisateur.php
+++ Wishlist/Utilisateur.php
@@ -1,1 +1,33 @@
 <?php
+class Utilisateur{
+	
+	private $pseudo;
+	private $nom;
+	private $prenom;
+	private $ville;
+	private $mail;
+	private $permission;
+	private $confidentialite;
+	private $dateNaissance;
+	private $photo;
+	
+	public function __construct($pseudo,$nom,$prenom,$ville,$mail,$permission,$confidentialite,$dateNaissance,$photo) {
+		$this->confidentialite = $confidentialite;
+		$this->dateNaissance = $dateNaissance;
+		$this->mail = $mail;
+		$this->permission = $permission;
+		$this->nom = $nom;
+		$this->photo = $photo;
+		$this->prenom = $prenom;
+		$this->pseudo = $pseudo;
+		$this->ville = $ville;
+	}
+	
+	public function getPseudo()
+	{
+		return $this->pseudo;
+	}
+}
+
+
+?>
