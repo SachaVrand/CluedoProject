27d9cd8871b502e4d242dad1b3f8a99d2d3575c0
IceXs <utilisateur@IceXs>
2015-12-09 16:05:52 +0100

    menu

---
+++ Wishlist/menuPrincipal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <meta http-equiv="Content-Language" content="fr" />
+	    <link rel="Stylesheet" type="text/css" href="menuPrincipal.css" />
+	</head>
+	
+	<ul id="menuPrincipal">
+		<li><a href="accueil.php">Logo</a></li>
+		<li><a href="recherche.php">Recherche</a></li>
+		<li><a href="accueil.php">Accueil</a></li>
+		<li><a href="infoPerso.php">Profil</a>
+			<ul>
+				<li><a href="infoPerso.php">Informations personnelles</a></li>
+				<li><a href="listeCadeau.php">Listes de cadeaux</a></li>
+				<li><a href="follow.php">Follow</a></li>
+			</ul>
+		</li>
+		<li><a href="messagerie.php">Messagerie</a></li>
+		<li><a href="notifications.php">Notifications</a></li>
+	</ul>
+</html>
