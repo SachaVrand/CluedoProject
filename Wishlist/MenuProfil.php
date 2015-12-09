76207d8734a064c332da1de69e91cffd584cd122
Sacha <vrand.sacha@gmail.com>
2015-12-09 14:38:45 +0100

    modif

---
+++ Wishlist/MenuProfil.php
@@ -0,0 +1,11 @@
+<nav id="menuProfil">        
+    <div class="element_menu">
+        <h3>Profil</h3>
+        <ul>
+            <li><a href="afaire">Lien</a></li>
+            <li><a href="afairel">Lien</a></li>
+            <li><a href="afaire">Lien</a></li>
+            <li><a href="afaire">Lien</a></li>
+        </ul>
+    </div>    
+</nav>
---
+++ Wishlist/menuInfosPerso.php
@@ -0,0 +1,11 @@
+<nav id="menuInfosPerso">        
+    <div class="element_menu">
+        <h3>Informations personnelles</h3>
+        <ul>
+            <li><a href="afaire">Lien</a></li>
+            <li><a href="afairel">Lien</a></li>
+            <li><a href="afaire">Lien</a></li>
+            <li><a href="afaire">Lien</a></li>
+        </ul>
+    </div>    
+</nav>
