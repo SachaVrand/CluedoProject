7654ec9ac7ebd72fd58d385d685188b8834ea683
Sacha <vrand.sacha@gmail.com>
2015-12-10 18:08:23 +0100

    modif

---
+++ Wishlist/pageConnexion.php
@@ -0,0 +1,196 @@
+<?php
+	function chargerClasse($classe)
+	{
+		require $classe . '.php';
+	}
+	spl_autoload_register('chargerClasse');
+	session_start();
+	
+	if(!isset($_SESSION['Connexion']))
+	{
+		$db = new ConnexionBase();
+		$_SESSION['Connexion'] = $db;
+	}
+	
+	if(isset($_POST['loginConnexion']) && isset($_POST['passwordConnexion']))
+	{
+		$user = Utilisateur::getUser($_SESSION['Connexion'], $_POST['loginConnexion'], $_POST['passwordConnexion']);
+		if($user == null)
+		{
+			$erreur = 'Mot de passe ou identifiant incorrect';
+		}
+		else
+		{
+			$_SESSION['user'] = $user;
+			//TODO changer la page
+			header("Location: mapage.php");
+			exit();
+		}
+	}
+	
+	if(isset($_POST['pseudo']))
+	{
+		//ajouter user
+		if($_POST['password'] === $_POST['cpassword'])
+		{
+			$motDePasse = $_POST['password'];
+			if(isset($_POST['photo']))
+			{
+				$photo = $_POST['photo'];
+			}
+			else
+			{
+				//TODO changer l'url
+				$photo = 'urlImageDeBase';
+			}
+			$user = new Utilisateur(Utilisateur::getNewId($_SESSION['Connexion']),$_POST['pseudo'], $_POST['nom'], $_POST['prenom'], $_POST['ville'], $_POST['mail'],0, $_POST['confidentialite'], $_POST['annee'].'-'.$_POST['mois'].'-'.$_POST['jour'], $photo);
+			$res = Utilisateur::addUserToDataBase($_SESSION['Connexion'], $user, $motDePasse);
+			if(!$res)
+			{
+				$existingLogin = 'Il existe deja un utilisateur avec le meme login';
+			}
+			else 
+			{
+				$notif = 'Votre inscription à bien été pris en compte.';
+			}
+		}
+		else
+		{
+			$erreurMdp = 'Les deux mots de passe ne correspondent pas';
+		}
+	}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+	<head>
+		<meta name="author" content="page de connexion">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="Stylesheet" type="text/css" href="pageConnexion.css" />
+		<title>
+		</title>
+	</head>
+	<header class="headerConnexion">
+		<!-- Connexion -->
+		<form action="pageConnexion.php" method="post">
+			<ul>
+				<li><label class="label">Login : </label></li>
+				<li><input type="text" name="loginConnexion" required="required" class="text"></li>
+				<li>
+				<?php
+					if(isset($erreur))
+					{
+						echo "$erreur";
+					}
+				?>
+				</li>
+			</ul>
+			<ul>
+				<li><label class="label">Mot de passe : </label></li>
+				<li><input type="password" name="passwordConnexion" required="required" class="text"></li>
+			</ul>
+			<ul>
+				<li> </li>
+				<li><input type="submit" name="Connexion" value="Connexion"></li>
+			</ul>
+		</form>
+	</header>
+	<body>
+	<section>
+		<form method="post" action="pageConnexion.php">
+			<?php
+				if(isset($notif))
+				{
+					echo "$notif";
+				}
+			?>
+			<table>
+				<tr>
+					<th colspan="2">Inscription</th>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="text" name="pseudo" required="required" placeholder="Pseudonyme"></td>
+				</tr>
+				<?php
+					if(isset($existingLogin))
+					{
+						//TODO afficher en rouge
+						echo "<tr><td colspan='2'>$existingLogin</td></tr>";
+					}
+				?>
+				<tr>
+					<td colspan="2"><input type="password" name="password" title="password" required="required" placeholder="Mot de passe"></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="password" name="cpassword" title="confirm password" required="required" placeholder="Entrez à nouveau"></td>
+				</tr>
+				<?php
+					if(isset($erreurMdp))
+					{
+						echo "<tr><td colspan='2'>$erreurMdp</td></tr>";
+					}
+				?>
+				<tr>
+					<td><input type="text" name="prenom" required="required" placeholder="Prenom"></td>
+					<td><input type="text" name="nom" required="required" placeholder="Nom"></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="text" name="ville" required="required" placeholder="Ville"></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="text" name="mail" required="required" placeholder="Adresse mail"></td>
+				</tr>
+				<tr>
+					<td colspan="2">Souhaitez vous que les autres utilisateurs puissent voir vos listes ?</td>
+				</tr>
+				<tr>
+					<td><input type="radio" name="confidentialite" value="1" id="oui" checked="checked" /> <label for="oui">Oui</label></td>
+					<td><input type="radio" name="confidentialite" value="0" id="non" /> <label for="non">Non</label></td>
+				</tr>
+				<tr>
+					<td>Date de naissance : </td>
+				</tr>
+				<tr>
+					<td>
+						<select name="jour">
+							<option value="1" selected="selected">1</option>
+							<?php
+								for($i = 2; $i <= 31; $i++)
+								{
+									echo "<option value=$i> $i </option>";
+								}
+							?>
+						</select>
+						<select name="mois">
+							<option value="1" selected="selected">1</option>
+							<?php 
+								for($i=2;$i<=12;$i++)
+								{
+									echo "<option value=$i> $i </option>";
+								}
+							?>
+						</select>
+					</td>
+					<td>
+						<select name="annee">
+							<?php
+								$y = date('Y');
+								echo "<option value=$y selected=selected> $y </option>";
+								for($i=$y-1 ; $i>$y-100 ; $i--)
+								{
+									echo "<option value=$i> $i </option>";
+								}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="text" name="photo" placeholder="URL de la photo de profil"></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="submit" name="Inscription" value="Inscription"></td>
+				<tr>
+			</table>
+		</form>
+	</section>
+	</body>
+</html>
