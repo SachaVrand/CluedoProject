64d0a239803ade600554167c8ecd9cd2dc6bca10
IceXs <utilisateur@IceXs>
2015-12-10 19:53:00 +0100

    menu

--- Wishlist/menuPrincipal.php
+++ Wishlist/menuPrincipal.php
@@ -8,44 +8,26 @@
 	
 	<header id="headerMenu">
 		<br />
-		<ul id="menuPrincipal">
-			<li><a href="accueil.php">W.</a></li>
-			<li><input type="text" name="recherche" id="recherche" placeholder="Cherchez des personnes" size="50"/></li>
-			<li><a href="resultat.php">Ok</a></li>
-			<li><a href="accueil.php">Accueil</a></li>
-			<li><a href="infoPerso.php">Profil personnel</a>
-				<ul>
-					<li><a href="infoPerso.php">Informations</a></li>
-					<li><a href="listeCadeau.php">Listes</a></li>
-					<li><a href="follow.php">Follow</a></li>
-				</ul>
-			</li>
-			<li><a href="messagerie.php">Messagerie</a></li>
-			<li><a href="notifications.php">Notifications</a></li>
-		</ul>
+		<table id="menuPrincipal">
+			<tr>
+				<td><a href="accueil.php">W.</a></td>
+				<td></td>
+				<td><input type="text" name="recherche" id="recherche" placeholder="Cherchez des personnes" size="50"/></td>
+				<td><input type="button" name="ok" value="Ok" style="height:26px"></td>
+				<td></td>
+				<td><a href="accueil.php">Accueil</a></td>
+				<td><a href="infoPerso.php">Profil personnel</a>
+					<ul>
+						<li><a href="infoPerso.php">Informations</a></li>
+						<li><a href="listeCadeau.php">Listes</a></li>
+						<li><a href="follow.php">Follow</a></li>
+					</ul>
+				</td>
+				<td><a href="messagerie.php">Messagerie</a></td>
+				<td><a href="notifications.php">Notifications</a></td>
+			</tr>
+		</table>
 		<br />
 	</header>
-	
-	<nav>
-		<ul>
-        	<li><a href="#">Accueil</a></li>
-			<li><a href="#">Blog</a></li>
-			<li><a href="#">CV</a></li>
-		</ul>
-	</nav>
-	<section>
-		<aside>
-			<h1>À propos de l'auteur</h1>
-			<p>C'est moi, Zozor ! Je suis né un 23 novembre 2005.</p>
-		</aside>
-		<article>                
-			<h1>Je suis un grand voyageur</h1>
-			<p>Bla bla bla bla (texte de l'article)</p>
-		</article>
-	</section>
-	<footer>
-		<p>Copyright Zozor - Tous droits réservés<br />
-		<a href="#">Me contacter !</a></p>
-	</footer>
 	
 </html>
