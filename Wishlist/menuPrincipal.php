8d5f3526194b45afa05fd98ea8c5e47884c3ce10
IceXs <utilisateur@IceXs>
2015-12-10 17:17:49 +0100

    menu

--- Wishlist/menuPrincipal.php
+++ Wishlist/menuPrincipal.php
@@ -5,11 +5,13 @@
 	    <meta http-equiv="Content-Language" content="fr" />
 	    <link rel="Stylesheet" type="text/css" href="menuPrincipal.css" />
 	</head>
+	
 	<header id="headerMenu">
 		<br />
 		<ul id="menuPrincipal">
 			<li><a href="accueil.php">W.</a></li>
-			<li><input type="text" name="recherche" id="recherche" placeholder="Cherchez des personnes" size="30"/></li>
+			<li><input type="text" name="recherche" id="recherche" placeholder="Cherchez des personnes" size="50"/></li>
+			<li><a href="resultat.php">Ok</a></li>
 			<li><a href="accueil.php">Accueil</a></li>
 			<li><a href="infoPerso.php">Profil personnel</a>
 				<ul>
@@ -23,4 +25,27 @@
 		</ul>
 		<br />
 	</header>
+	
+	<nav>
+		<ul>
+        	<li><a href="#">Accueil</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">CV</a></li>
+		</ul>
+	</nav>
+	<section>
+		<aside>
+			<h1>À propos de l'auteur</h1>
+			<p>C'est moi, Zozor ! Je suis né un 23 novembre 2005.</p>
+		</aside>
+		<article>                
+			<h1>Je suis un grand voyageur</h1>
+			<p>Bla bla bla bla (texte de l'article)</p>
+		</article>
+	</section>
+	<footer>
+		<p>Copyright Zozor - Tous droits réservés<br />
+		<a href="#">Me contacter !</a></p>
+	</footer>
+	
 </html>
