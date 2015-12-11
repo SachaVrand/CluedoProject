66fc3477cbf8ae95c736289899492dac970ec283
IceXs <utilisateur@IceXs>
2015-12-11 02:28:34 +0100

    menu trop SWAG !!!

--- Wishlist/menuPrincipal.php
+++ Wishlist/menuPrincipal.php
@@ -5,29 +5,49 @@
 	    <meta http-equiv="Content-Language" content="fr" />
 	    <link rel="Stylesheet" type="text/css" href="menuPrincipal.css" />
 	</head>
-	
-	<header id="headerMenu">
-		<br />
-		<table id="menuPrincipal">
+	<body>
+		<header id="headerMenu">
+			<br />
+			<table id="menuPrincipal">
+				<tr>
+					<td><a href="accueil.php">W.</a></td>
+					<td></td>
+					<td><input type="text" name="recherche" id="recherche" placeholder="Cherchez des personnes" size="50"/></td>
+					<td><input type="button" name="ok" value="Ok"></td>
+					<td></td>
+					<td><a href="accueil.php">Accueil</a></td>
+					<td><a href="infoPerso.php">Profil personnel</a>
+						<ul>
+							<li><a href="infoPerso.php">Informations</a></li>
+							<li><a href="listeCadeau.php">Listes</a></li>
+							<li><a href="follow.php">Follow</a></li>
+						</ul>
+					</td>
+					<td><a href="messagerie.php">Messagerie</a></td>
+					<td><a href="notifications.php">Notifications</a></td>
+				</tr>
+			</table>
+			<br />
+		</header>
+		
+		<table id="principal" width=44%  height=50%>
 			<tr>
-				<td><a href="accueil.php">W.</a></td>
-				<td></td>
-				<td><input type="text" name="recherche" id="recherche" placeholder="Cherchez des personnes" size="50"/></td>
-				<td><input type="button" name="ok" value="Ok"></td>
-				<td></td>
-				<td><a href="accueil.php">Accueil</a></td>
-				<td><a href="infoPerso.php">Profil personnel</a>
-					<ul>
-						<li><a href="infoPerso.php">Informations</a></li>
-						<li><a href="listeCadeau.php">Listes</a></li>
-						<li><a href="follow.php">Follow</a></li>
-					</ul>
+				<td width=15%>
+					<section id="sousMenu">
+						<ul>
+							<li><a href="infoPerso.php">Informations</a></li>
+							<li><a href="listeCadeau.php">Listes</a></li>
+							<li><a href="follow.php">Follow</a></li>
+						</ul>
+					</section>
+				</td width=85%>
+				<td>
+					<section id="content">
+					ghghshrhth
+					</section>
 				</td>
-				<td><a href="messagerie.php">Messagerie</a></td>
-				<td><a href="notifications.php">Notifications</a></td>
 			</tr>
 		</table>
-		<br />
-	</header>
+	</body>
 	
 </html>
