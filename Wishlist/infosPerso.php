e801c6e0dac896d6cde07bfd47f2208f5d10b47e
IceXs <utilisateur@IceXs>
2015-12-11 18:48:47 +0100

    image

--- Wishlist/infosPerso.php
+++ Wishlist/infosPerso.php
@@ -13,7 +13,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="Content-Language" content="fr" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	    <link rel="Stylesheet" type="text/css" href="interfaceAvecMenu.css" />
 		<title>Informations personnelles</title>
 	</head>
@@ -43,23 +43,21 @@
 						<?php
 							$user = $_SESSION['user'];
 							$date = explode('-', $user->dateNaissance);
-							$months = array('Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre');
+							$months = array('janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','decembre');
 							$month = $date[1]-1;
 							//TODO changer l'affichage pour l'image
 							echo 
-							"<table>
-								<tr> <td rowspan=2>Image</td> <td>$user->pseudo</td> <td></td> </tr>
-								<tr> <td></td> <td>$user->nom $user->prenom, </td> <td>$user->mail</td> </tr>
-							</table>";
-							echo '<br>';
-							echo 
-							"<table>
-								<tr><td>Ville : </td><td>$user->ville</td></tr>
-								<tr></tr>
-								<tr><td>Naissance : </td> <td>$date[2]</td> <td>$months[$month] </td> <td>$date[0]</td> </tr>
-							</table>";
+							'<table>
+								<tr> <td rowspan=2>Image</td> <td colspan=2>'.$user->pseudo.'</td> </tr>
+								<tr> <td>'.$user->nom.' '.$user->prenom.', </td> <td>'.$user->mail.'</td> </tr>
+							</table>'.
+							'<br>'.
+							'Ville : '.$user->ville.
+							'<br>'.
+							'Naissance : '.$date[2].' '.$months[$month].' '.$date[0].
+							'<br><br>';
 						?>
-						<button type="button" onclick="self.location.href='modifInfosPerso.php'">Modifier vos informations</button>
+						<button type="button" onclick="self.location.href='modifInfosPerso.php'">Modifier</button>
 					</div>
 				</td>
 			</tr>
