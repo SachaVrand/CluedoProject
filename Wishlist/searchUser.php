80f2d22101195c1624a2ccdb785ed04302dbc464
Sacha <vrand.sacha@gmail.com>
2015-12-12 17:12:43 +0100

    modif

--- Wishlist/searchUser.php
+++ Wishlist/searchUser.php
@@ -34,66 +34,55 @@
 		<div id="titre">
 			Profil de <?php echo $_GET['user']; ?>
 		</div>
-		
+		<?php 
+			if(!$userToDisplay || $userToDisplay->confidentialite == 1 || Utilisateur::isRestrict($_SESSION['Connexion'], $user, $userToDisplay))
+			{
+				?>
+					<div id="contentSansMenu">
+					<?php
+						if(!$userToDisplay)
+						{
+							echo "Aucun utilisateur n'a pu etre trouve";
+						}
+						else if($userToDisplay->confidentialite === 1)
+						{
+							echo "L'utilisateur ne souhaite pas que l'on puisse avoir acces a son profil";
+						}
+						else if(Utilisateur::isRestrict($_SESSION['Connexion'], $user, $userToDisplay))
+						{
+							echo "L'utilisateur ne souhaite pas que vous puissiez acceder a son profil.";
+						}
+					?>
+					</div>
+				<?php
+				exit();
+			}
+		?>
 		<table id="principal">
 			<tr>
-				<?php 
-					if(!(!$userToDisplay || $userToDisplay->confidentialite == 1 || Utilisateur::isRestrict($_SESSION['Connexion'], $user, $userToDisplay)))
-					{
-				?>
 				<td>
 					<div id="sousMenu">
 						<ul>
-							<?php
-								
-							?>
 							<li><a href="#">Not implemented</a></li>
 						</ul>
 					</div>
 				</td>
-				<?php
-					}
-				?>
 				<td>
-					<div id=<?php 
-								if(!$userToDisplay || $userToDisplay->confidentialite == 1 || Utilisateur::isRestrict($_SESSION['Connexion'], $user, $userToDisplay))
-								{
-									echo '"ContentSansMenu"';
-								}
-								else
-								{
-									echo '"ContentAvecMenu"';
-								}
-							?> >
+					<div id="contentAvecMenu">
 						<?php
-							if(!$userToDisplay)
-							{
-								echo "Aucun utilisateur n'a pu etre trouve";
-							}
-							else if($userToDisplay->confidentialite === 1)
-							{
-								echo "L'utilisateur ne souhaite pas que l'on puisse avoir acces a son profil";
-							}
-							else if(Utilisateur::isRestrict($_SESSION['Connexion'], $user, $userToDisplay))
-							{
-								echo "L'utilisateur ne souhaite pas que vous puissiez acceder a son profil.";
-							}
-							else
-							{
-								$date = explode('-', $userToDisplay->dateNaissance);
-								$months = array('janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','decembre');
-								$month = $date[1]-1;
-								echo
-								'<table>
-									<tr> <td rowspan=2><img src="'.$userToDisplay->photo.'" /></td> <td colspan=2>'.$userToDisplay->pseudo.'</td> </tr>
-									<tr> <td>'.$userToDisplay->nom.' '.$userToDisplay->prenom.', </td> <td>'.$userToDisplay->mail.'</td> </tr>
-								</table>'.
-								'<br>'.
-								'Ville : '.$userToDisplay->ville.
-								'<br>'.
-								'Naissance : '.$date[2].' '.$months[$month].' '.$date[0].
-								'<br><br>';
-							}				
+							$date = explode('-', $userToDisplay->dateNaissance);
+							$months = array('janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','decembre');
+							$month = $date[1]-1;
+							echo
+							'<table>
+								<tr> <td rowspan=2><img src="'.$userToDisplay->photo.'" /></td> <td colspan=2>'.$userToDisplay->pseudo.'</td> </tr>
+								<tr> <td>'.$userToDisplay->nom.' '.$userToDisplay->prenom.', </td> <td>'.$userToDisplay->mail.'</td> </tr>
+							</table>'.
+							'<br>'.
+							'Ville : '.$userToDisplay->ville.
+							'<br>'.
+							'Naissance : '.$date[2].' '.$months[$month].' '.$date[0].
+							'<br><br>';			
 						?>
 					</div>
 				</td>
